1c510e280264909660c2920837ab55432734f43d
Brandon Villa <bornofos@gmail.com>
2017-10-18 14:51:44 -0500

    Ingredientes component

---
+++ src/app/cuenta/ingredientes-cuenta/ingredientes-cuenta.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-ingredientes-cuenta',
+    template: `<router-outlet></router-outlet>`
+})
+
+export class IngredientesCuentaComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
