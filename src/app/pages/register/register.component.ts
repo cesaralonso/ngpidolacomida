ef373f64da160c4f3470cf98422bf1fef5f329da
Brandon Villa <bornofos@gmail.com>
2017-10-18 17:54:37 -0500

    Register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,3 +1,9 @@
+import { RolInterface } from './../../shared/models/rol.model';
+import { RolService } from './../../shared/services/rol.service';
+import { UserInterface } from './../../shared/models/user.model';
+import { TelefonoInterface } from './../../shared/models/telefono.model';
+import { UserService } from './../../shared/services/user.service';
+import { TelefonoService } from './../../shared/services/telefono.service';
 import { Router } from '@angular/router';
 import { PersonResponseInterface } from './person.response.interface';
 import { PersonInterface } from './person.interface';
@@ -17,20 +23,33 @@ export class RegisterComponent {
 
   public form: FormGroup;
   public name: AbstractControl;
+  public apellidos: AbstractControl;
   public email: AbstractControl;
   public telefono: AbstractControl;
+  public lada: AbstractControl;
+  public compania: AbstractControl;
   public password: AbstractControl;
   public repeatPassword: AbstractControl;
   public passwords: FormGroup;
 
   public submitted = false;
-
-  constructor(fb: FormBuilder, private registerService: RegisterService, private router: Router) {
+  public roles: RolInterface[];
+  constructor(
+      fb: FormBuilder,
+      private registerService: RegisterService,
+      private router: Router,
+      private telefonoService: TelefonoService,
+      private userService: UserService,
+      private rolService: RolService
+    ) {
 
     this.form = fb.group({
       'name': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      'apellidos': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
       'email': ['', Validators.compose([Validators.required, EmailValidator.validate])],
       'telefono': ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(15)])],
+      'lada': ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(15)])],
+      'compania': ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(15)])],
       'passwords': fb.group({
         'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
         'repeatPassword': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
@@ -38,26 +57,40 @@ export class RegisterComponent {
     });
 
     this.name = this.form.controls['name'];
+    this.apellidos = this.form.controls['apellidos'];
     this.email = this.form.controls['email'];
     this.telefono = this.form.controls['telefono'];
+    this.lada = this.form.controls['lada'];
+    this.compania = this.form.controls['compania'];
     this.passwords = <FormGroup> this.form.controls['passwords'];
     this.password = this.passwords.controls['password'];
     this.repeatPassword = this.passwords.controls['repeatPassword'];
 
+    // Get the rol data
+    rolService.all().subscribe( res => this.roles = res.result );
   }
 
   public onSubmit(values: any): void {
-    this.submitted = true;
-    if (this.form.valid) {
-      const person: PersonInterface = {
-        email: values.email,
-        password: values.passwords.password,
-        name: values.name
-      }
-      this.registerService.addUser(person)
-        .subscribe(
-            (response: PersonResponseInterface) => this.showModal(response));
+    const telefono: TelefonoInterface = {
+      numero: values.telefono,
+      lada: values.lada,
+      compania: values.compania
+    };
+    const user: UserInterface = {
+      nombres: '',
+      apellidos: '',
+      email: '',
+      password: '',
+      telefono_idtelefono: '',
+      rol_idrol: ''
+    };
+    if ( this.form.valid ) {
+      this.telefonoService.create( telefono )
+        .flatMap( res => this.userService.create( user ))
+        .subscribe( res => console.log(res));
+
     }
+
   }
 
   private showModal(response: PersonResponseInterface) {
