2b424ad213dd6fd11613934d959c33d7beef9c61
Brandon Villa <bornofos@gmail.com>
2017-10-19 18:22:23 -0500

    service

---
+++ src/app/shared/services/restaurante-platillo.service.ts
@@ -0,0 +1,31 @@
+import { RestaurantePlatilloInterface } from './../models/restaurante-platillo.model';
+import { Observable } from 'rxjs/Observable';
+import { Http, Headers } from '@angular/http';
+import { BasicRequestsService } from './basic-requests.service';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RestaurantePlatilloService extends BasicRequestsService {
+
+  constructor ( protected http: Http ) {
+    super(http, '/restaurantePlatillo');
+  }
+
+  public create( restaurantePlatillo: RestaurantePlatilloInterface ): Observable<any> {
+    restaurantePlatillo.tiempopreparacion =
+      `00:${restaurantePlatillo.tiempopreparacion.getHours()}:${restaurantePlatillo.tiempopreparacion.getMinutes()}`;
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    return this.http.post(`${this.endPoint}`, restaurantePlatillo, { headers: headers })
+      .map( res => res.json() || {})
+      .catch( error => JSON.stringify(error));
+  }
+
+  public update( restaurantePlatillo: RestaurantePlatilloInterface ): Observable<any> {
+    restaurantePlatillo.tiempopreparacion =
+    `00:${restaurantePlatillo.tiempopreparacion.getHours()}:${restaurantePlatillo.tiempopreparacion.getMinutes()}`;
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    return this.http.patch(`${this.endPoint}`, restaurantePlatillo, { headers: headers })
+      .map( res => res.json() || {})
+      .catch( error => JSON.stringify(error));
+  }
+}
