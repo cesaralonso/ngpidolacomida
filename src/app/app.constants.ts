17b362aba49207910e5071b92286f620c4d906d3
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2017-10-18 19:49:35 -0500

    constants merge

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Configuration {
 
-  public Server: string = 'http://pidolacomidalb.herokuapp.com/';
-  public ApiUrl: string = 'api/';
+  // public Server: string = 'http://pidolacomidalb.herokuapp.com/';
+  public Server: string = 'http://localhost/pidolacomidaslim/';
+  public ApiUrl: string = 'api/v1/';
   public ServerWithApiUrl = this.Server + this.ApiUrl;
 
 }
