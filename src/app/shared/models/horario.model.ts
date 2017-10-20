f37a85142d4e397738a8f839a49aa86d0373c415
Brandon Villa <bornofos@gmail.com>
2017-10-20 13:47:36 -0500

    Models and services for horarios

---
+++ src/app/shared/models/horario.model.ts
@@ -0,0 +1,6 @@
+export interface HorarioInterface {
+    idhorario: string;
+    hora_ini: string;
+    hora_fin: string;
+    semana_idsemana: string;
+}
---
+++ src/app/shared/models/restaurante-horario.model.ts
@@ -0,0 +1,5 @@
+export interface RestauranteHorarioInterface {
+    restaurante_idrestaurante: string;
+    horario_idhorario: string;
+    horario_semana_idsemana: string;
+}
