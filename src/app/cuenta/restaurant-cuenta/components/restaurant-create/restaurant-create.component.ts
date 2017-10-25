f96c174dca429a1800f9261bed905ca3226a74ff
Brandon Villa <bornofos@gmail.com>
2017-10-25 16:01:07 -0500

    Restaurant form view

--- src/app/cuenta/restaurant-cuenta/components/restaurant-create/restaurant-create.component.ts
+++ src/app/cuenta/restaurant-cuenta/components/restaurant-create/restaurant-create.component.ts
@@ -13,23 +13,55 @@ export class RestaurantCreateComponent implements OnInit {
 
   public nombre: AbstractControl;
   public razonSocial: AbstractControl;
-  public rfc: AbstractControl;
-  public direccion: AbstractControl;
   public descripcion: AbstractControl;
+  public calle: AbstractControl;
+  public entrecalle1: AbstractControl;
+  public entrecalle2: AbstractControl;
+  public lat: AbstractControl;
+  public lng: AbstractControl;
+  public numint: AbstractControl;
+  public numext: AbstractControl;
+  public colonia: AbstractControl;
+  public cp: AbstractControl;
+  public principal: AbstractControl;
+  public ciudad: AbstractControl;
+
+  public ciudades = [
+    'uno',
+    'dos'
+  ];
 
   constructor( formBuilder: FormBuilder ) {
     this.formGroup = formBuilder.group({
       'nombre': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
       'razonSocial': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
-      'rfc': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
-      'direccion': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
       'descripcion': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      'calle': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      'entrecalle1': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      'entrecalle2': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      'lat': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      'lng': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      'numint': ['', Validators.compose([Validators.required, Validators.minLength(1)])],
+      'numext': ['', Validators.compose([Validators.required, Validators.minLength(1)])],
+      'colonia': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      'cp': ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(5)])],
+      'principal': '',
+      'ciudad': ['', Validators.required]
     });
     this.nombre = this.formGroup.controls['nombre'];
     this.razonSocial = this.formGroup.controls['razonSocial'];
-    this.rfc = this.formGroup.controls['rfc'];
-    this.direccion = this.formGroup.controls['direccion'];
     this.descripcion = this.formGroup.controls['descripcion'];
+    this.calle = this.formGroup.controls['calle'];
+    this.entrecalle1 = this.formGroup.controls['entrecalle1'];
+    this.entrecalle2 = this.formGroup.controls['entrecalle2'];
+    this.lat = this.formGroup.controls['lat'];
+    this.lng = this.formGroup.controls['lng'];
+    this.numint = this.formGroup.controls['numint'];
+    this.numext = this.formGroup.controls['numext'];
+    this.colonia = this.formGroup.controls['colonia'];
+    this.cp = this.formGroup.controls['cp'];
+    this.principal = this.formGroup.controls['principal'];
+    this.ciudad = this.formGroup.controls['ciudad'];
 
   }
 
