d2e978b0b5f87a210ce7225a4548200f67be3fd8
Brandon Villa <bornofos@gmail.com>
2017-10-29 12:43:05 -0600

    Combo create brings the platillos related to the restaurante on select tag

--- src/app/cuenta/combo-cuenta/components/combo-create/combo-create.component.ts
+++ src/app/cuenta/combo-cuenta/components/combo-create/combo-create.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute } from '@angular/router';
+import { RestaurantePlatilloService } from './../../../../shared/services/restaurante-platillo.service';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
@@ -5,13 +7,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-combo-create',
   templateUrl: './combo-create.component.html',
-  styleUrls: ['./combo-create.component.css']
+  styleUrls: ['./combo-create.component.css'],
+  providers: [
+    RestaurantePlatilloService
+  ]
 })
 export class ComboCreateComponent implements OnInit {
   public tituloHeader = 'Crear un nuevo combo';
   public textColor = '#444';
+  public restauranteId: string;
   public selectedPlatillo: any;
   public platillos = [];
+  public platillosFromAPI: any[] = [];
   // Form validation
   public formGroup: FormGroup;
   public titulo: AbstractControl;
@@ -19,7 +26,14 @@ export class ComboCreateComponent implements OnInit {
   public precio: AbstractControl;
   public platillo: AbstractControl;
 
-  constructor( formBuilder: FormBuilder ) {
+  constructor(
+    formBuilder: FormBuilder,
+    activatedRoute: ActivatedRoute,
+    private restaurantePlatilloService: RestaurantePlatilloService
+  ) {
+    activatedRoute.params.subscribe( parameters => {
+      this.restauranteId = parameters['restauranteId'];
+    })
     this.formGroup = formBuilder.group({
       'titulo': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
       'descripcion': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
@@ -31,12 +45,15 @@ export class ComboCreateComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getPlatillosFromAPI()
   }
-
+  getPlatillosFromAPI() {
+    this.restaurantePlatilloService.getByParam( 'restaurante_idrestaurante', this.restauranteId)
+      .subscribe( res => res.success ? this.platillosFromAPI = res.result : null)
+  }
   onChangePlatillo( platillo ) {
     this.selectedPlatillo = platillo;
   }
-
   addPlatillo() {
     if ( this.selectedPlatillo !== undefined && this.selectedPlatillo !== '') {
       this.platillos.push(this.selectedPlatillo);
