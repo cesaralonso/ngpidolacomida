34806d25a684caa2cf668d5bb9c30239c5c0c6dd
Brandon Villa <bornofos@gmail.com>
2017-11-03 15:56:19 -0600

    Methods added for service

--- src/app/shared/services/platillo-ingrediente.service.ts
+++ src/app/shared/services/platillo-ingrediente.service.ts
@@ -10,6 +10,29 @@ export class PlatilloIngredienteService extends BasicRequestsService {
     super(http, '/platilloIngrediente', true);
   }
 
+
+  public allIngredientes( restauranteId, platilloId ): Observable<any> {
+    return this.http.get(`${this.endPoint}/${restauranteId}/${platilloId}`)
+      .map(res => res.json() || { } )
+      .catch(err =>  JSON.stringify(err));
+  }
+
+  public findById( id ): Observable<any> {
+    return this.http.get(`${this.endPoint}/${id}`)
+      .map( res => res.json() || {} )
+      .catch( error => JSON.stringify(error) );
+  }
+
+  public create( model ): Observable<any> {
+    const headers = new Headers({ 'Content-Type': 'application/json'});
+    if ( this.requireAuthentication ) {
+      headers.append('Authorization', JSON.parse(localStorage.getItem('pidelacomida.token')));
+    }
+    return this.http.post(`${this.endPoint}`, model, { headers: headers })
+      .map( res => res.json() || {})
+      .catch( error => JSON.stringify(error));
+  }
+
   public remove( id ): Observable<any> {
     const headers = new Headers({ 'Content-Type': 'application/json'});
     headers.append('Authorization', JSON.parse(localStorage.getItem('pidelacomida.token')));
